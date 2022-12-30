0fb41cd1e5b90630dfe9b8a13567f95538edf015
nikkylu <nikkylu@foxmail.com>
2022-12-30 00:37:40 -0500

    embed

--- src/Embed.js
+++ src/Embed.js
@@ -6,7 +6,7 @@ const Embed = ({ embedId }) => (
     <iframe
       width="853"
       height="480"
-      src={`https://youtu.be/embedId${embedId}`}
+      src={`https://www.youtube.com/embed/embedId${embedId}`}
       frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowFullScreen
